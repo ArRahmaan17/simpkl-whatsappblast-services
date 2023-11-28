d5850a7905a3d7d9914ffcdf599a07fcfe1c3c83
RahmaanM17 <ardrah17@gmail.com>
2023-11-28 23:00:00 +0700

    feat: remove port on servicess

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const qrcode = require('qrcode-terminal');
 const app = express()
-// const port = 3000;
+const port = 3000;
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     puppeteer: {
