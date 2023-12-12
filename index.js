89c095218d85288a7fc102c12da58ed4faf3352b
RahmaanM17 <ardrah17@gmail.com>
2023-12-12 14:52:42 +0700

    feat: no get single contact on bot

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         const extension = file.originalname.split('.')[1]
-        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}.${extension}`;
+        const date = new Date();
+        const uniqueSuffix = `${date.getDate()}${date.getMonth()}${date.getFullYear()}.${extension}`;
         cb(null, file.fieldname + '-' + uniqueSuffix)
     }
 })
@@ -62,56 +63,65 @@ client.on('ready', async () => {
         let mentions = [];
         let chats = await client.getChats();
         chats = chats.find(chat => { return chat.name == 'PKL Nug Creative' });
+        // chats = chats.find(chat => { return chat.name == 'Nitip Bro' });
         phone_numbers = JSON.parse(req.body.phone_number);
         for (let participant of chats.groupMetadata.participants) {
             if (participant.id.user != '6285173007324' && phone_numbers.includes(`${participant.id.user.split('62').join('')}`)) {
-                const contact = await client.getContactById(participant.id._serialized);
                 replyMessage += `@${participant.id.user} \n`;
-                mentions.push(contact)
+                mentions.push(participant.id._serialized)
             }
         }
         replyMessage += 'Mohon untuk segera melakukan absensi di https://sim-pkl.nugcreative.my.id/';
         if (mentions.length == 0) {
             replyMessage = `Semua Telah Melakukan Absensi.\n Semangat pkl hari ini 🔥🔥🔥`;
         }
-        chats.sendMessage(replyMessage, { mentions });
+        chats.sendMessage(replyMessage, { mentions: mentions });
         res.send({ 'status': 'Success', 'Message': "Success send notification for all users" });
     });
     // Attendance Warning for user
-    app.get('/attendance-warning/:phone_number', upload.array(), async (req, res) => {
-        const contact = await client.getContactById(`62${req.params.phone_number}@c.us`);
-        client.sendMessage(`62${req.params.phone_number}@c.us`, `Di informasikan kepada *62${req.params.phone_number}* untuk segera melakukan presensi https://sim-pkl.nugcreative.my.id/`);
-        res.send({ 'status': 'Success', 'Message': "Success send notification for" + req.params.phone_number });
-    });
+    // app.get('/attendance-warning/:phone_number', upload.array(), async (req, res) => {
+    //     client.sendMessage(`62${req.params.phone_number}@c.us`, `Di informasikan kepada *62${req.params.phone_number}* untuk segera melakukan presensi https://sim-pkl.nugcreative.my.id/`);
+    //     res.send({ 'status': 'Success', 'Message': "Success send notification for" + req.params.phone_number });
+    // });
     // Attendance Success for user
-    app.get('/attendance-success/:phone_number/:status', upload.array(), async (req, res) => {
-        const contact = await client.getContactById(`62${req.params.phone_number}@c.us`);
-        client.sendMessage(`62${req.params.phone_number}@c.us`, `Hai *${contact.name}*, Anda telah berhasil melakukan presensi ${req.params.status}`);
+    app.post('/attendance-success/:phone_number/:status', upload.single('file_attendance'), async (req, res) => {
+        let chats = await client.getChats();
+        chats = chats.find(chat => { return chat.name == 'PKL Nug Creative' });
+        // chats = chats.find(chat => { return chat.name == 'Nitip Bro' });
+        let replyMessage = '';
+        let mentions = [];
+        for (let participant of chats.groupMetadata.participants) {
+            if (participant.id.user != '6285173007324' && req.params.phone_number == `${participant.id.user.split('62').join('')}`) {
+                replyMessage += `@${participant.id.user} telah melakukan absensi ${req.params.status}`;
+                mentions.push(participant.id._serialized)
+            }
+        }
+        const media = MessageMedia.fromFilePath(req.file.path);
+        chats.sendMessage(media, { mentions: mentions, caption: replyMessage });
         res.send({ 'status': 'Success', 'Message': "Success send notification for" + req.params.phone_number });
     });
-    app.post('/attendance-notification/:phone_number/:student_phone_number', upload.single('file_attendance'), async (req, res) => {
-        const student_contact = await client.getContactById(`62${req.params.student_phone_number}@c.us`);
-        const media = MessageMedia.fromFilePath(req.file.path);
-        client.sendMessage(`62${req.params.phone_number}@c.us`, media, { caption: `62${req.params.student_phone_number} melakukan absensi` });
-        res.send({ 'status': 'Success', 'Message': "Success send notification for " + req.params.phone_number });
-    });
+
+    // app.post('/attendance-notification/:phone_number/:student_phone_number', upload.single('file_attendance'), async (req, res) => {
+    //     const media = MessageMedia.fromFilePath(req.file.path);
+    //     client.sendMessage(`62${req.params.phone_number}@c.us`, media, { caption: `62${req.params.student_phone_number} melakukan absensi` });
+    //     res.send({ 'status': 'Success', 'Message': "Success send notification for " + req.params.phone_number });
+    // });
     app.post('/task-notification', upload.single('task_thumbnail'), async (req, res) => {
         let chats = await client.getChats();
         chats = chats.find(chat => { return chat.name == 'PKL Nug Creative' });
+        // chats = chats.find(chat => { return chat.name == 'Nitip Bro' });
         phone_numbers = JSON.parse(req.body.phone_numbers);
         let replyMessage = '';
         let mentions = [];
         for (let participant of chats.groupMetadata.participants) {
             if (participant.id.user != '6285173007324' && phone_numbers.includes(`${participant.id.user.split('62').join('')}`)) {
-                const contact = await client.getContactById(participant.id._serialized);
                 replyMessage += `@${participant.id.user} \n`;
-                mentions.push(contact)
+                mentions.push(participant.id._serialized)
             }
         }
         replyMessage += `\nDiinformasikan tugas baru tentang *${req.body.title}* telah tiba🤣`
         const media = MessageMedia.fromFilePath(req.file.path);
-        chats.sendMessage(media, { mentions: mentions, caption: replyMessage })
-        // client.sendMessage(`62${req.params.phone_number}@c.us`, media, { caption: `${contact.name} melakukan absensi` });
+        chats.sendMessage(media, { mentions: mentions, caption: replyMessage });
         res.send({ 'status': 'Success', 'Message': "Success send notification for all user in group" });
     });
 });
